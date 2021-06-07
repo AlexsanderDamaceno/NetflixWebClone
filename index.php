76e5531ace578fd4dc8e732db3af444a80c04da3
Alexsander Borges Damaceno <aemgbo@gmail.com>
2021-06-07 20:45:40 -0300

    first cry of netflix.
    Co-authored-by: Antonio Sidnei Moreira Cirqueira <tonisidneimc@hotmail.com>

---
+++ index.php
@@ -0,0 +1,4 @@
+hello world
+rggf
+sfd
+g
---
+++ register.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+
+<html> 
+
+<head> 
+ <title> Welcome to  DamaFlix</title>
+ <link rel="stylesheet" type="text/css" href="assets/style/style.css">
+</head> 
+
+<body> 
+    test 
+</body> 
+
+</html>
